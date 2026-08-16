824f0b731a87c8b2e6bdc47c35d6d34237feec9d
shaik khaleed <khaleedshaik27@gmail.com>
2026-08-16 22:11:08 +0530

    Time: 4 ms (26.37%), Space: 44 MB (30.2%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+
+        if (s.length() != goal.length()) return false;
+
+        int a = -1, b = -1;
+        boolean[] seen = new boolean[26];
+        boolean duplicate = false;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) != goal.charAt(i)) {
+                if (a == -1) a = i;
+                else if (b == -1) b = i;
+                else return false;
+            }
+
+            int x = s.charAt(i) - 'a';
+            if (seen[x]) duplicate = true;
+            seen[x] = true;
+        }
+
+        // Strings are same
+        if (a == -1)
+            return duplicate;
+
+        // Exactly 2 mismatches
+        return b != -1 &&
+               s.charAt(a) == goal.charAt(b) &&
+               s.charAt(b) == goal.charAt(a);
+    }
+}
