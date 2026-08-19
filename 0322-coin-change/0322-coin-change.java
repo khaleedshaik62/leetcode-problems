002288443cf4c590eadb88eee3b34890ca0d77cc
shaik khaleed <khaleedshaik27@gmail.com>
2026-08-19 10:10:30 +0530

    Time: 16 ms (69.22%), Space: 46.1 MB (93.2%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -3,16 +3,25 @@ class Solution {
 
         int[] dp = new int[amount + 1];
 
+        // Initially, assume every amount is impossible
         Arrays.fill(dp, amount + 1);
 
+        // 0 amount needs 0 coins
         dp[0] = 0;
 
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
-                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        for (int i = 1; i <= amount; i++) {
+
+            for (int coin : coins) {
+
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
             }
         }
 
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
+        if (dp[amount] == amount + 1)
+            return -1;
+
+        return dp[amount];
     }
 }
